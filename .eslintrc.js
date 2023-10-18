0494681beee81332e2b9d721554cc95c14d02f67
agustinibar <agustinibarperrotta@gmail.com>
2023-10-18 14:03:44 -0300

    linter

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+    },
+    extends: ['react-app', 'react-app/jest'],
+    rules: {
+      'no-unused-vars': 'warn', // Tratar advertencias como advertencias
+    },
+  };
+  
